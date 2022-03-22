1ecbc0a3b987834bcc743743909d7f72ebbf0c86
19690148 <NDStarchenko@sberbank.ru>
2022-03-22 23:36:42 +0300

    create healthcheck

--- framework/framework.go
+++ framework/framework.go
@@ -6,6 +6,7 @@ import (
 
 func Start(port string, function http.HandlerFunc) error {
 	http.HandleFunc("/", function)
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("OK")) })
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		return err
